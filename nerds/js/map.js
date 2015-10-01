a6822952c59d13f79772c834f3d1795b41bd1cd9
Татьяна <taniakiral@mail.ru>
2015-10-01 11:56:01 +0300

    Финальная версия с результатом 100%

--- nerds/js/map.js
+++ nerds/js/map.js
@@ -3,7 +3,7 @@ function initialize() {
     zoom: 17,
     center: new google.maps.LatLng(59.9391150,30.3200000)
   }
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
+  var map = new google.maps.Map(document.getElementById("map-canvas"),
                                 mapOptions);
   var image = "img/map-marker.png";
   var myLatLng = new google.maps.LatLng(59.9386900,30.3238200);
@@ -13,4 +13,4 @@ function initialize() {
     icon: image
   });
 }
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, "load", initialize);
